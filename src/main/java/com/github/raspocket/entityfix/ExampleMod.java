d81f2e5ff9784492389910da0748f1b52bbc12db
GitHub Action <action@github.com>
2025-05-06 12:02:54 +0000

    Initialize template repository proper
    Set package name to com.github.raspocket.entityfix
    Set project name to EntityFix
    Set modid to entityfix

--- src/main/java/com/github/raspocket/entityfix/ExampleMod.java
+++ src/main/java/com/github/raspocket/entityfix/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.raspocket.entityfix;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "entityfix", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {

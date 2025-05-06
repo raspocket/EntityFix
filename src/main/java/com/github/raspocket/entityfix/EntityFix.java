127be41ce954cf37678f130fd51ece74afde1835
raspocket <uweganser74@gmail.com>
2025-05-06 14:15:08 +0200

    Update EntityFix.java

--- src/main/java/com/github/raspocket/entityfix/EntityFix.java
+++ src/main/java/com/github/raspocket/entityfix/EntityFix.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = "fgtxray", name = "EntityFix", useMetadata=true)
-public class ExampleMod {
+public class EntityFix {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());

3a43332a2be603a4ce47e0770d197f5bfdb1a7a6
raspocket <uweganser74@gmail.com>
2025-05-06 14:06:03 +0200

    Update MixinGuiMainMenu.java

--- src/main/java/com/github/raspocket/entityfix/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/raspocket/entityfix/mixin/MixinGuiMainMenu.java
@@ -1,16 +1,26 @@
 package com.github.raspocket.entityfix.mixin;
 
-import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mixin(GuiMainMenu.class)
-public class MixinGuiMainMenu {
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+    @Unique
+    private static final Logger soapClient$LOGGER = LogManager.getLogger("ItemStackMixin");
 
-    @Inject(method = "initGui", at = @At("HEAD"))
-    public void onInitGui(CallbackInfo ci) {
-        System.out.println("Hello from Main Menu!");
+    @Inject(method = "hasEffect", at = @At("HEAD"), cancellable = true)
+    private void wrapHasEffect(CallbackInfoReturnable<Boolean> cir) {
+        try {
+            cir.setReturnValue(((ItemStack) (Object) this).getItem().hasEffect((ItemStack) (Object) this));
+        } catch (Exception e) {
+            soapClient$LOGGER.info("Error in hasEffect: ", e);
+            cir.setReturnValue(false);
+        }
     }
 }
